eb1e91ac827de636988a4864827e4373c3e92097
ed20jr <ed20jr@leeds.ac.uk>
2025-06-17 20:06:12 +0100

    Add navigation

--- app/src/main/java/com/example/recipevault/MainActivity.kt
+++ app/src/main/java/com/example/recipevault/MainActivity.kt
@@ -6,18 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,6 +29,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.recipevault.ui.theme.RecipeVaultTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,22 +41,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeVaultTheme {
-                Scaffold(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    content = { innerPadding ->
-                        Surface(
-                            modifier = Modifier
-                                .padding(innerPadding)
-                                .fillMaxSize()
-                        ) {
-                            HomeView(
-                                modifier = Modifier
-                                    .padding(horizontal = 16.dp)
-                            )
-                        }
-                    }
-                )
+                RecipeVaultApp()
+
             }
         }
     }
@@ -74,9 +68,17 @@ fun ImagePlaceholder(modifier: Modifier = Modifier, text: String) {
 }
 
 @Composable
-fun RecipeCard(modifier: Modifier, title: String, desciption: String, image: Image?) {
+fun RecipeCard(
+    modifier: Modifier,
+    title: String,
+    description: String,
+    image: Image?,
+    onClick: () -> Unit
+) {
     Card(
-        modifier = modifier, elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        modifier = modifier.clickable { onClick() },
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+
     ) {
         if (image == null) {
             ImagePlaceholder(text = title.plus(" (image was null)"))
@@ -91,7 +93,7 @@ fun RecipeCard(modifier: Modifier, title: String, desciption: String, image: Ima
         )
         Text(
             modifier = Modifier.padding(12.dp),
-            text = desciption,
+            text = description,
             style = MaterialTheme.typography.bodyMedium
         )
     }
@@ -99,25 +101,61 @@ fun RecipeCard(modifier: Modifier, title: String, desciption: String, image: Ima
 }
 
 @Composable
-fun HomeView(modifier: Modifier) {
+fun HomeView(modifier: Modifier, navController: NavHostController) {
     val items = List(50) { "Recipe ${it + 1}" }
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = modifier.padding(end = 0.dp)
+        modifier = modifier
     ) {
         items(items) { item ->
             RecipeCard(
                 modifier = Modifier.fillMaxSize(),
                 title = item,
-                desciption = "Lorem Ipsum decourm est lorem ipsum decoum est loreum ipsum decorum est In Compose, use a serializable object or class to define a route. A route describes how to get to a destination, and contains all the information that the destination requires.",
-                image = null
+                description = "Lorem Ipsum decourm est lorem ipsum decoum est loreum ipsum decorum est In Compose, use a serializable object or class to define a route. A route describes how to get to a destination, and contains all the information that the destination requires.",
+                image = null,
+                onClick = { navController.navigate("recipe") }
+            )
+        }
+
+    }
+}
+
+@Composable
+fun RecipeView(modifier: Modifier, navController: NavHostController) {
+    Column(modifier = modifier) {
+        Text(text = "Recipe View")
+        Button(onClick = { navController.navigateUp() }) { Text(text = "Go Back") }
+    }
+
+
+}
+
+@Composable
+fun RecipeVaultApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeView(
+                modifier = Modifier
+                    .padding(WindowInsets.safeDrawing.asPaddingValues())
+                    .padding(horizontal = 8.dp)
+                    .padding(top = 12.dp)
+                    .fillMaxSize(),
+                navController = navController
+            )
+        }
+        composable("recipe") {
+            RecipeView(
+                modifier = Modifier
+                    .padding(WindowInsets.safeDrawing.asPaddingValues())
+                    .padding(horizontal = 8.dp)
+                    .padding(top = 12.dp)
+                    .fillMaxSize(),
+                navController = navController
             )
         }
     }
 }
 
-@Composable
-fun RecipeView(modifier: Modifier) {
 
 
-}
