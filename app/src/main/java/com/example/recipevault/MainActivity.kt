fd1ba874e76a1377853e917a77e529f7d133ff51
ed20jr <ed20jr@leeds.ac.uk>
2025-06-17 18:42:09 +0100

    Remove nav for now, add serif font

--- app/src/main/java/com/example/recipevault/MainActivity.kt
+++ app/src/main/java/com/example/recipevault/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -39,47 +40,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             RecipeVaultTheme {
-                NavHost(navController= navController, startDestination = )
-                Scaffold(modifier = Modifier.fillMaxSize().padding(WindowInsets.safeDrawing.asPaddingValues())) { innerPadding ->
-                    HomeView(modifier = Modifier.padding(innerPadding).padding(16.dp))
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(WindowInsets.safeDrawing.asPaddingValues())) { innerPadding ->
+                    HomeView(modifier = Modifier
+                        .padding(innerPadding)
+                        .padding(16.dp))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-
-    RecipeVaultTheme {
-        Greeting("Android")
-    }
-}
 
 @Composable
 fun ImagePlaceholder(modifier: Modifier = Modifier, text: String){
     Box(
         contentAlignment = Alignment.Center,
-        modifier = modifier.fillMaxSize().aspectRatio(1f).background(Color.LightGray),
+        modifier = modifier
+            .fillMaxSize()
+            .aspectRatio(1f)
+            .background(Color.LightGray),
     ){
-        Text(text=text, color = Color.DarkGray, style = MaterialTheme.typography.displayLarge)
+        Text(text=text, color = Color.DarkGray, style = MaterialTheme.typography.displayLarge, textAlign = TextAlign.Center)
     }
 }
 
 @Composable
-fun RecipeCard(modifier: Modifier, title: String, image: Image?){
+fun RecipeCard(modifier: Modifier, title: String, desciption: String, image: Image?){
     Card (modifier = modifier){
-        ImagePlaceholder(text = title)
+        if (image == null) {
+            ImagePlaceholder(text = title.plus(" (image was null)"))
+        } else {
+            ImagePlaceholder(text = title)
+        }
+
         Text(modifier = Modifier.padding(12.dp), text=title, style = MaterialTheme.typography.displaySmall)
+        Text(modifier = Modifier.padding(12.dp), text=desciption, style = MaterialTheme.typography.bodyMedium)
     }
 
 }
@@ -89,7 +87,7 @@ fun HomeView( modifier: Modifier){
     val items  = List(50) { "Recipe ${it + 1}" }
     LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp), modifier = modifier){
         items(items){
-            item -> RecipeCard(modifier = Modifier.fillMaxSize(), title =item, image = null)
+            item -> RecipeCard(modifier = Modifier.fillMaxSize(), title =item, desciption = "Lorem Ipsum decourm est lorem ipsum decoum est loreum ipsum decorum est In Compose, use a serializable object or class to define a route. A route describes how to get to a destination, and contains all the information that the destination requires.", image = null)
         }
     }
 }
--- app/src/main/java/com/example/recipevault/ui/theme/Type.kt
+++ app/src/main/java/com/example/recipevault/ui/theme/Type.kt
@@ -1,10 +1,38 @@
 package com.example.recipevault.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.recipevault.R
+
+val EBGaramond = FontFamily(
+    Font(R.font.eb_garamond, weight = FontWeight.Normal)
+)
+
+
+val customTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = EBGaramond,
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = EBGaramond,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = EBGaramond,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    )
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
